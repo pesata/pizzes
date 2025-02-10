1dc01506720a6ba5fb8e7d9a5c2ea12757730656
450426.clot@fje.edu <450426.clot@fje.edu>
2025-02-10 21:05:51 +0000

    Commit 4 de l’aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -11,7 +11,8 @@
             if (empty($_GET['ingr'])) {
                 $pvp = ((100 + $IVA) / 100) * $preu;
                 echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
-                exit(0);
+                //exit(0);
+
             } else {
                 $IVA = 21;
                 $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
@@ -23,11 +24,11 @@
                     }
                 }
                 echo "El preu és $pvp € (IVA inclòs)<br>";
-                exit(0);
+                //exit(0);
             }
 
             echo "<br>";
-            echo "Autor: dacomo" . "<br>";
+            echo "Autor: pesata" . "<br>";
         ?>
     </body>
 </html>
