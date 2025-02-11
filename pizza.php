d12a0e61d20f44391b9f275619c2954b284258bc
450426.clot@fje.edu <450426.clot@fje.edu>
2025-02-11 23:55:48 +0000

    Commit 6.1 de l'aplicatiu pizzes

--- pizza.php
+++ pizza.php
@@ -28,7 +28,7 @@
             }
 
             echo "<br>";
-            echo "Autor: pesata 2.1" . "<br>";
+            echo "Autor: pesata 2.3" . "<br>";
         ?>
          <a href="pizza.html">Retorna al formulari de selecció d’ingredients</a>
     </body>
